1700aa38d883285f676785f519a46ea749c15031
Shun Cheung <shunkongcheung@gmail.com>
2022-01-08 12:10:37 -0800

    feat(hello): add isAuth

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getIsAuth } from "../../utils";
 
 interface Data {
   name: string;
+  isAuth: boolean;
+  password: string;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const password = req.body.password || "";
+  const isAuth = getIsAuth(password);
+
+  res.status(200).json({ name: "John Doe", isAuth, password });
 }
