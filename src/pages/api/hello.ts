0463d2e8b54834b53a4c57e1b749fea38733b468
Shun Cheung <shunkongcheung@gmail.com>
2022-01-09 12:35:58 -0800

    feat(hello): remove isAuth

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,19 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getIsAuth } from "../../utils";
 
 interface Data {
   name: string;
-  isAuth: boolean;
-  password: string;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const password = req.body.password || "";
-  const isAuth = getIsAuth(password);
-
-  res.status(200).json({ name: "John Doe", isAuth, password });
+  res.status(200).json({ name: "John Doe" });
 }
